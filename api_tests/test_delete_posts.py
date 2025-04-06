0fb9b52c6a645a8567e73dbd1fa781dbd42d6b70
khudeeva <kskhudeeva@gmail.com>
2025-04-06 17:59:18 +0700

    Добавлен негативный тест по test_delete_posts

--- api_tests/test_delete_posts.py
+++ api_tests/test_delete_posts.py
@@ -10,4 +10,10 @@ def test_delete_post_sucess():
 def test_delete_post_parametrized(post_id):
     response = delete_post(post_id)
     assert response.status_code == 200
-    # assert response.text == "{}"
+    assert response.text == "{}"
+
+# негативный тест - удаление несуществующего поста
+def test_delete_post_non_exisiting_post():
+    response = delete_post(9999)
+    assert response.status_code !=500
+    assert response.text == "{}"
